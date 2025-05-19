75e54043a8352e76d1114a4430ebb37c1bf28ee1
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-05-19 22:04:23 +0400

    add UiTextField component

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,21 +1,25 @@
 import {authControllerGetSessionInfo} from "@/shared/api/generated";
 import {useQuery} from "@tanstack/react-query";
 import {UiButton} from "@/shared/ui/ui-button";
+import {UiTextField} from "@/shared/ui/ui-text-field";
 
 export const HomePage = () => {
 
-  const { data } = useQuery({
-    queryKey: ['session'],
-    queryFn: () => authControllerGetSessionInfo()
-  });
+    const {data} = useQuery({
+        queryKey: ['session'],
+        queryFn: () => authControllerGetSessionInfo()
+    });
 
-  return (
-      <main className={'flex min-h-screen flex-col items-center justify-between p-24'}>
-        { data?.email }
-        <UiButton variant="primary">Test</UiButton>
-        <UiButton variant="secondary">Test</UiButton>
-        <UiButton variant="outlined">Test</UiButton>
-        <UiButton disabled variant="primary">Test</UiButton>
-      </main>
-  )
+    return (
+        <main className={'flex min-h-screen flex-col items-center justify-between p-24'}>
+            {data?.email}
+            <UiButton variant="primary">Test</UiButton>
+            <UiButton variant="secondary">Test</UiButton>
+            <UiButton variant="outlined">Test</UiButton>
+            <UiButton disabled variant="primary">Test</UiButton>
+            <UiTextField label={'Text field'} inputProps={{placeholder: 'Enter email'}}></UiTextField>
+            <UiTextField error={'Text field'} inputProps={{placeholder: 'Enter email'}}></UiTextField>
+            <UiTextField inputProps={{placeholder: 'Enter email'}}></UiTextField>
+        </main>
+    )
 }
