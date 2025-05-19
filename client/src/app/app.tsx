ffe596cde3479f925ee5d5f12a92972fb20a987b
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-05-19 21:26:48 +0400

    add provider

--- client/src/app/app.tsx
+++ client/src/app/app.tsx
@@ -1,6 +1,9 @@
 import "@/app/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
+import {AppProvider} from "@/app/app-provider";
 
-export const App = ({ Component, pageProps }: AppProps) => {
-    return <Component {...pageProps} />;
+export const App = ({Component, pageProps}: AppProps) => {
+    return <AppProvider>
+        <Component {...pageProps} />
+    </AppProvider>;
 }
