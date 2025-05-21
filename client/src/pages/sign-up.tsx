d0ca444c9be04bdefa5615b6c6bb36b4d798dd30
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-05-21 20:56:58 +0400

    add UiFormPageLayout

--- client/src/pages/sign-up.tsx
+++ client/src/pages/sign-up.tsx
@@ -1,16 +1,9 @@
 import {SignUpForm} from "@/features/auth";
 import {UiHeader} from "@/shared/ui/ui-header";
+import {UiFormPageLayout} from "@/shared/ui/layouts/ui-form-page-layout";
 
 export const SignUpPage = () => {
     return (
-        <div className={"min-h-screen flex flex-col bg-slate-100"}>
-            <UiHeader/>
-            <main className={"grow flex flex-col pt-24"}>
-                <div className={"rounded-xl border border-slate-300 px-14 py-8 pb-14 w-full max-w-[400px] bg-white self-center"}>
-                    <h1 className={"text-2xl mb-6"}>Sing Up</h1>
-                    <SignUpForm/>
-                </div>
-            </main>
-        </div>
+        <UiFormPageLayout title={"Sign Up"} header={<UiHeader/>} form={<SignUpForm/>}/>
     );
 }
